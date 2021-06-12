8a472292973a679136dd526897b45c1a8e150215
arovcanin1 <73299629+arovcanin1@users.noreply.github.com>
2021-06-12 09:39:47 +0200

    n

--- CryptoInvestt/CryptoInvestt/Models/Valuta.cs
+++ CryptoInvestt/CryptoInvestt/Models/Valuta.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,23 @@ namespace CryptoInvestt.Models
     public class Valuta
     {
         public int ID { get; set; }
+
+        public string Naziv { get; set; }
+        public double TrenutnaVrijednost { get; set; }
+
+        [NotMapped]
+        public List<double> DnevneVrijednosti { get; set; }
+
+        [NotMapped]
+        public List<double> SedmicneVrijednosti { get; set; }
+
+        [NotMapped]
+        public List<double> MjesecneVrijednosti { get; set; }
+
+        [NotMapped]
+        public List<double> GodisnjeVrijednosti { get; set; }
+
+        public double Max24h { get; set; }
+        public double Min24h { get; set; }
     }
 }
